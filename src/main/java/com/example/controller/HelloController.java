7a2ddde222eaec2df771326ec186aa66f0af4368
Anupam Kumar <anupam.nitdgp14@gmail.com>
2026-05-27 20:38:19 +0530

    Added health controller.

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,11 @@ public class HelloController {
     @GetMapping
     public String basicHello(){
         return "Hello World!";
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> checkHealth(){
+        return ResponseEntity.ok("Health is ok");
     }
 
     @GetMapping("/docker")
