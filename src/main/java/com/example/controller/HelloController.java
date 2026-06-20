ae53bbe0f467b512addad4cdad43ad642b026e93
Anupam Kumar <anupam.nitdgp14@gmail.com>
2026-06-20 22:27:53 +0530

    Added logs.

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,18 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping
     public String basicHello(){
+        log.info("Hello World");
         return "Hello World!";
     }
 
     @GetMapping("/health")
     public ResponseEntity<String> checkHealth(){
+        log.info("Checking health. Health is OK");
         return ResponseEntity.ok("Health is ok");
     }
 
     @GetMapping("/docker")
     public String sayHello(){
+        log.info("Hello World from Docker Application");
         return "Hello World! from Docker Application";
     }
 }
